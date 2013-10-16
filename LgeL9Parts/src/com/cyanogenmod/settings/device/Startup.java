0df09c902a12a3170ed706e11917613009e6cfb6
patryson <patryson82@gmail.com>
2013-10-16 23:12:53 +0200

    Add advanced settings

--- LgeL9Parts/src/com/cyanogenmod/settings/device/Startup.java
+++ LgeL9Parts/src/com/cyanogenmod/settings/device/Startup.java
@@ -8,7 +8,6 @@ public class Startup extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
-        GeneralFragment.restore(context);
         GpuOverclock.restore(context);
         IvaOverclock.restore(context);
         MaxSleepFrequency.restore(context);
