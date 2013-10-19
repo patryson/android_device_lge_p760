3a8a265358584cceec89a4828c77e90f25b0b5a4
patryson <patryson82@gmail.com>
2013-10-19 10:31:21 +0200

    Add advanced settings

--- LgeL9Parts/src/com/cyanogenmod/settings/device/GpuOverclock.java
+++ LgeL9Parts/src/com/cyanogenmod/settings/device/GpuOverclock.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2012 The CyanogenMod Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright (C) 2012 The CyanogenMod Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package com.cyanogenmod.settings.device;
 
@@ -37,7 +37,7 @@ public class GpuOverclock implements OnPreferenceChangeListener {
         }
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-        Utils.writeValue(FILE, sharedPrefs.getString(DeviceSettings.KEY_GPU_OVERCLOCK, "1"));
+        Utils.writeValue(FILE, sharedPrefs.getString(PerformanceFragment.KEY_GPU_OVERCLOCK, "1"));
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
